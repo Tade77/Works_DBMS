3bc5ba279040100c9addb614695bdb1eb0f85a9f
Tade77 <adekunletaofeeq@mail.com>
2024-08-26 10:16:42 +0100

    feat: Integrating user profile with image

--- ProEstate/urls.py
+++ ProEstate/urls.py
@@ -27,9 +27,11 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
     path('profile/', user_view.userProfile, name='user-profile'),
-
     path('', auth_view.LoginView.as_view(template_name='user_template/login.html'), name='user-login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='user_template/logout.html'), name='user-logout')
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- user/form.py
+++ user/form.py
@@ -10,13 +10,9 @@ CAMPUSES = [
 
 class createUserForm(UserCreationForm):
     email = forms.EmailField()
-    department = forms.CharField(max_length=100)
-    school = forms.CharField(max_length=200)
-    campus = forms.MultipleChoiceField(choices=CAMPUSES)
-    phone = forms.CharField(max_length=11)
-    firstname = forms.CharField(max_length=200)
-    lastname = forms.CharField(max_length=200)
+    
+
     
     class Meta:
         model = User
-        fields = ['firstname', 'lastname','email', 'phone' ,'department','school', 'username', 'campus', 'password1', 'password2']
+        fields = ['username','email', 'password1', 'password2']
